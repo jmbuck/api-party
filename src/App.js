3d301d7aeb87526a0cd190e6f7d318100b33e7f5
Jordan Buckmaster <buckmast@purdue.edu>
2017-06-20 15:25:33 -0400

    Change header content and style

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="App-header">
+          <div className="App-headings">
+            <h3>Ain't no party like an</h3>
+            <h1>API Party</h1>
+          </div>
+        </div>
       </div>
     );
   }

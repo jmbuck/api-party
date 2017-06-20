3a91f6d1434747dc1aef06bab0072bf27ad67e1d
Jordan Buckmaster <buckmast@purdue.edu>
2017-06-20 15:37:48 -0400

    Create GitHub component and NavLinks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Switch, Route, NavLink } from 'react-router-dom'
+import GitHub from './GitHub'
 import './App.css';
 
 class App extends Component {
@@ -11,6 +13,19 @@ class App extends Component {
             <h1>API Party</h1>
           </div>
         </div>
+        <ul className="nav-links">
+          <li>
+            <NavLink to="/github">GitHub API</NavLink>
+          </li>
+          <li>
+            <NavLink to="/nasa">NASA API</NavLink>
+          </li>
+        </ul>
+        <Switch>
+          <Route path='/github' component={GitHub} />
+          <Route path='/nasa' render={() => <h1>NASA</h1>} />
+          <Route render={() => <p>To get started, click one of the links above!</p>} />
+        </Switch>
       </div>
     );
   }
---
+++ src/GitHub.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class GitHub extends Component {
+    render() {
+        return <h1>GitHub Component</h1>
+    }
+}
+
+export default GitHub
